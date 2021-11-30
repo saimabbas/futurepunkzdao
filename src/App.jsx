21c05ea7af79954aaa4e346a2d1e560ab81c6016
Saim Abbas <saimabbas220022@gmail.com>
2021-11-30 16:39:07 +0500

    Changes

--- src/App.jsx
+++ src/App.jsx
@@ -23,11 +23,16 @@ import Story6 from "./assets/Story6.png";
 import Story7 from "./assets/Story7.png";
 import Story8 from "./assets/Story8.png";
 import Story9 from "./assets/Story9.png";
+import PixDus from "./assets/PixDus.png";
+import Hunter from "./assets/69Hunter.png";
+import Olof from "./assets/Olof888.png";
+import Solo from "./assets/Solo Monk.png";
+import NU from "./assets/NU.png";
 import Specie10 from "./assets/Specie10.png";
 import Icon1 from "./assets/icon1.svg";
 import Icon3 from "./assets/icon3.svg";
 import Space from "./assets/black.jpg";
-import HeroBanner from "./assets/hero-banner.jpg";
+import HeroBanner from "./assets/website-banner-2.jpg";
 import Logo from "./assets/logo.png";
 
 import { Power4, Linear } from "gsap/dist/gsap";
@@ -339,53 +344,34 @@ function App() {
               <h1 className="nebula">Battle for Utopia</h1>
               <p>
                 In the mountains of Nepal, a cave was discovered, holding the
-                story of 4 legendary Monkz…
-                <br />
-                It is said that they were so wise, they created a decentralized
-                city of financial freedom and abundance called Utopia. In
-                Utopia, people lived in harmony with their finances,
-                decentralized from authority.
+                story of 4 legendary Monkz… <br />
+                It is said that they were so wise, they created a city where
+                people lived in harmony with their finances, decentralized from
+                authority. UTOPIA. <br />
                 <br />
                 The true history of the Monkz has long been forgotten, but
                 legends and rumors of a portal that leads to their world have
                 been part of many cultures’ lore for thousands of years. Some
-                call it the vault of time, the valley of forever, or just the
-                Eternal.
+                call it the vault of time. Every time the vault opens, a war
+                breaks out between the world’s most powerful tribes to claim the
+                right to enter the lost valley of WGMI and gain access to the
+                secret riches of Utopia.
                 <br />
-                Every time the portal opens, a war breaks out between the
-                world’s most powerful tribes to claim the right to enter the
-                lost valley and gain access to the secret riches of Utopia.
                 <br />
                 In the year 4444, the portal opened and war broke out once
-                again.
+                again. Two great factions rose, The Psychic Punkz and the
+                Alchemist Punkz. Which tribe will make it to the city of Utopia
+                in the Monkz metaverse?
                 <br />
-                Two great factions rose, The Psychic Punkz and the Alchemist
-                Punkz. Which faction will win out and make it to the city of
-                Utopia in the Monkz metaverse?
                 <br />
                 Psychic Punkz possess the power of seeing the short-term future
-                and therefore are excellent at fast decisions. They take high
-                risks and earn high rewards.
-                <br />
-                Alchemist Punkz rely on their experience and knowledge and
+                and therefore are excellent at fast decisions and flipping.
+                Alchemist Punkz rely on their patience and conviction and
                 therefore can calculate what projects will prevail long-term.
-                With the power of patience and conviction, they accumulate
-                slowly, but steadily, over time.
+                <br />
+                <br />
+                CHOOSE YOUR TRIBE.
               </p>
-              {/* <h1 className="nebula">CHOOSE YOUR TRIBE</h1>
-              <p>
-                Future Punkz Genesis is a collection of 4444 NFTs on the
-                Ethereum Blockchain. The holders gain access to DAO votes, the
-                DAO game, and future drops.
-                <br />
-                <br />
-                The first DAO battle of its kind.
-                <br />
-                <br />
-                An experiment to find out which investment strategy is the best
-                in the fast-moving NFT market. A friendly competition to create
-                the most value for our community.
-              </p> */}
             </div>
           </div>
         </div>
@@ -457,7 +443,7 @@ function App() {
             </p>
             <br />
             <br />
-           <h2>Let the battle begin! </h2>
+            <h2>Let the battle begin! </h2>
           </div>
         </div>
       </div>
@@ -472,52 +458,85 @@ function App() {
           <div className="roadmap-content">
             <div className="roadmap-container">
               <div className="roadmap-box">
-                <h4>Before mintstage</h4>
-                <p>➥ Open up discord server.</p>
-                <p>➥ Future Punkz giveaways on twitter.</p>
-                <p>➥ Collect whitelist spots.</p>
+                <h4>- LAUNCH -</h4>
+                <p>
+                  4444 Future Punkz Genesis will be released to the blockchain.
+                  <br />
+                  Alchemists, Psychics and Legendary Monkz enter the metaverse.
+                </p>
               </div>
               <main></main>
             </div>
             <div className="roadmap-container">
               <div className="roadmap-box">
-                <h4>During mintstage</h4>
-                <p>➥ Setting up of vault in fractional.art.</p>
-                <p>➥ First snapshot vote for NFT purchase.</p>
-                <p>➥ Staking preparation</p>
+                <h4>- THE DAO GAME -</h4>
+                <p>
+                  Psychics vs Alchemists! Flipping vs holding! Who is gonna make
+                  it? <br /> Which Bluechips - moonshots - crypto currencies -
+                  meta verse land do we get? <br /> Choose your tribe and pray
+                  for your victory!
+                </p>
               </div>
               <main></main>
             </div>
             <div className="roadmap-container">
               <div className="roadmap-box">
-                <h4>After 100% sellout</h4>
-                <p>➥ Add 100 Ethereum to the DAO wallet</p>
-                <p>➥ Thats 42% of all mint funds</p>
-                <p>➥ Make our first Bluechip NFT purchase </p>
-                <p>➥ Start implementing staking. </p>
+                <h4>- DONATION -</h4>
+                <p>
+                  Let’s make a donation from the team wallet to an organization
+                  of your choice.
+                  <br />
+                  We can also add something from the DAO wallet if you feel
+                  generous;)
+                </p>
               </div>
               <main></main>
             </div>
             <div className="roadmap-container">
               <div className="roadmap-box">
-                <h4>Secondary Sale</h4>
+                <h4>- NEXT LEVEL -</h4>
                 <p>
-                  ➥ <b>75%</b> of open sea royalties to the community wallet
-                </p>
-                <p>
-                  ➥ <b>25%</b> goes to development and staff.
-                </p>
-                <p>
-                  ➥ Tokenomics for <b>$JUICE</b> will be decided by the
-                  community.
-                </p>
-                <p>
-                  ➥ FuturePunkz Nft can be staked to earn <b>$JUICE.</b>
+                  Time to expand the battle. Let’s create something unique.
+                  <br />
+                  Future Punkz DAO members and artists will conspire to create
+                  the v2 anime style collection.
+                  <br />
+                  This collection will be the heart of this DAO cause we all
+                  contribute to it.
                 </p>
               </div>
               <main></main>
             </div>
-            <div className="roadmap-options">
+            <div className="roadmap-container">
+              <div className="roadmap-box">
+                <h4>- MERCH - </h4>
+                <p>Merch is merch. Let’s make something dope.</p>
+              </div>
+              <main></main>
+            </div>
+            <div className="roadmap-container">
+              <div className="roadmap-box">
+                <h4>- V2 - </h4>
+                <p>
+                  V2 to the world. Imagine the first community created bluechip
+                  Nft.
+                  <br />
+                  We are not afraid to dream big.
+                  <br />A part of these sales will also be allocated to the DAO
+                  wallet.
+                </p>
+              </div>
+              <main></main>
+            </div>
+            <div className="roadmap-container">
+              <div className="roadmap-box">
+                <h4>- UTOPIA ROADMAP 2.0 -</h4>
+                <p>
+                  Metaverse - launchpad - IRL events <br /> The future is DAO!
+                </p>
+              </div>
+            </div>
+            {/* <div className="roadmap-options">
               <div className="roadmap-options-ceiling"></div>
               <div className="roadmap-options-container">
                 <div className="roadmap-option-box">
@@ -549,7 +568,7 @@ function App() {
                   <div className="roadmap-option-text">Donation?</div>
                 </div>
               </div>
-            </div>
+            </div> */}
           </div>
         </div>
       </div>
@@ -559,22 +578,18 @@ function App() {
         <div className="box">
           <div className="about-content">
             <p>
-              We have many possibilities and ideas from here. But its essential
-              to a Dao that the community votes on which roads we take with the
-              project. When phase 1 is completed we will introduce a few options
-              for phase 2.
+              There will be a lot of discussing, exchanging, voting and
+              brainstorming. Together with the community we will create the best
+              roadmap for our DAO. These are some milestones on that road. But
+              we will stay dynamic and fluid with the times, with our evolution
+              and most importantly with what brings us joy and fulfillment.
             </p>
             <br />
             <p>
-              We recommend to hold at least 2 Future Punkz of 2 different
-              species until that point. Future Punkz Dao is a circle inside of a
-              system of multiple circles. We are excited to share the process
-              with you and can’t wait to build a longterm utility for our
-              members.{" "}
+              The destination is clear…
+              <br />
+              UTOPIA
             </p>
-            <br />
-            <p>FuturePunkz DAO = Key</p>
-            <p>Door coming soon. Hold.</p>
           </div>
         </div>
       </div>
@@ -588,7 +603,7 @@ function App() {
               <a href="https://twitter.com/NuMaSaCa1">
                 <img src={Icon3} alt="" />
               </a>
-              <img src={Specie1} alt="" />
+              <img src={NU} alt="" />
               <main>
                 <h6>Numa Saca</h6>
                 <h5>Founder </h5>
@@ -599,7 +614,7 @@ function App() {
               <a href="https://twitter.com/Solo2313137943">
                 <img src={Icon3} alt="" />
               </a>
-              <img src={Specie7} alt="" />
+              <img src={Solo} alt="" />
               <main>
                 <h6>Solo Monk</h6>
                 <h5>Founder </h5>
@@ -607,12 +622,12 @@ function App() {
               </main>
             </div>
             <div className="team-box">
-              <a href="https://twitter.com/HbHbHb83658985">
+              <a href="https://twitter.com/kacc28419807">
                 <img src={Icon3} alt="" />
               </a>
-              <img src={Specie6} alt="" />
+              <img src={PixDus} alt="" />
               <main>
-                <h6>Huzaifa</h6>
+                <h6>Pixdus</h6>
                 <h5>Artist</h5>
               </main>
             </div>
@@ -620,10 +635,21 @@ function App() {
               {/* <a href="#">
                 <img src={Icon3} alt="" />
               </a> */}
-              <img src={Specie3} alt="" />
+              <img src={Hunter} alt="" />
               <main>
                 <h6>69Hunter </h6>
                 <h5>Developer </h5>
+              </main>
+            </div>
+            <div className="team-box">
+              <a href="https://twitter.com/olof888">
+                <img src={Icon3} alt="" />
+              </a>
+              <img src={Olof} alt="" />
+              <main>
+                <h6>Olof888 </h6>
+                <h5>Discord Admin </h5>
+                <h5>Marketing Manager</h5>
               </main>
             </div>
           </div>
